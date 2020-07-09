09db4b77e17de729325246e326368ebc4d1a3ecd
Greg Guerin <GregGuerin@users.noreply.github.com>
2020-07-09 12:53:30 +0930

    Remove buffer function
    Switching from raster:buffer operation for buffers to using circles from dismo and  masking raster to each one sequentially as buffer function is selecting the whole raster for some reason

--- R/buffer.frags.R
+++ R/buffer.frags.R
@@ -24,9 +24,12 @@ buffer.frags <- function(XY, radius, vegetation.base.raster, plot=TRUE) {
 	}
 
 
+  buffers <- dismo:::.generateCircles(XY, d=radius, lonlat=longlat)
+  vegetation.base.raster[is.na(vegetation.base.raster)] <- 100
+  
+  
   if(plot){
     if(grep("longlat", projection(vegetation.base.raster)) == 1) {longlat=TRUE} else {longlat=FALSE}
-    buffers <- dismo:::.generateCircles(XY, d=radius, lonlat=longlat)
     plot(vegetation.base.raster)
     points(XY, pch=20)
     plot(buffers, add=TRUE)
@@ -36,15 +39,18 @@ buffer.frags <- function(XY, radius, vegetation.base.raster, plot=TRUE) {
 	dat <- list()
 	for(i in 1:length(LONG)) {
 		n <- n + 1
-		temp.rast <- vegetation.base.raster #copy
-		focal.cell <- cellFromXY(temp.rast, c(LONG[n], LAT[n])) #find raster cell associated with the site/coordinates
-		temp.rast[] <- NA
-		temp.rast[focal.cell] <- 1
-		temp.rast <- raster::buffer(temp.rast, width=radius)
-		outer.cells <- which(is.na(temp.rast[])) #vector of cells outside the buffer zone
-		temp.rast <- vegetation.base.raster #reset
-		temp.rast[is.na(temp.rast)] <- 100
-		temp.rast[outer.cells] <- NA
+		# temp.rast <- vegetation.base.raster #copy
+		# focal.cell <- cellFromXY(temp.rast, c(LONG[n], LAT[n])) #find raster cell associated with the site/coordinates
+		# temp.rast[] <- NA
+		# temp.rast[focal.cell] <- 1
+		# temp.rast <- raster::buffer(temp.rast, width=radius)
+		# outer.cells <- which(is.na(temp.rast[])) #vector of cells outside the buffer zone
+		# temp.rast <- vegetation.base.raster #reset
+		# temp.rast[is.na(temp.rast)] <- 100
+		# temp.rast[outer.cells] <- NA
+		# plot(temp.rast)
+		temp.rast <- mask(vegetation.base.raster, buffers[n])
+		plot(temp.rast)
 		dat[[n]] <- ClassStat(temp.rast)
 		}
 		try(names(dat) <- row.names(XY))
